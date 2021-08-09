604a35e105f07280cf04ce5dcb43031224e5b9c3
chethansh54 <trainerchetanh@gmail.com>
2021-08-09 10:19:28 +0530

    Day1Codes added

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+speed=10.3456
+classname = "Python Classes"
+classFinished = True
+
+another_speed=10.34567
+
+print(type(speed))
+print(type(classname))
+print(type(classFinished))
+print("speed ADDRESS: ",id(speed))
+print(id(classname))
+print(id(classFinished))
+print("another_speed ADDRESS : ",id(another_speed))
+
+
